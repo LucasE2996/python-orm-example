01c0ae88e754129a5c8d595a932f6dedcebd9f5b
LucasE2996 <moeda.60@gmail.com>
2019-03-29 22:58:20 -0300

    exc 2 done

--- newsdb.py
+++ newsdb.py
@@ -4,13 +4,23 @@ import psycopg2
 
 DBNAME = "news"
 
-def get_all_news():
-  """Return all news from the 'database'."""
+def get_top_3_articles():
+  """Return top 3 most accessed articles from the 'database'."""
   db = psycopg2.connect(database=DBNAME)
   c = db.cursor()
-  c.execute("select content, time from posts oder by time desc")
+  c.execute("select distinct count(log.time) as num, articles.title from log, articles where log.path like CONCAT('/article/', articles.slug) group by title order by num desc LIMIT 3;")
+  for i in c.fetchall():
+    print('"' + str(i[1]) + '" - ' + str(i[0]) + ' views')
   db.close()
-  return c.fetchall()
+
+def get_top_3_authors():
+  """Return top 3 most read authors from the 'database'."""
+  db = psycopg2.connect(database=DBNAME)
+  c = db.cursor()
+  c.execute("select distinct authors.name, sum(num) as total_views from views_sum join authors on views_sum.author_id = authors.id group by authors.name order by total_views desc LIMIT 3;")
+  for i in c.fetchall():
+    print('"' + str(i[0]) + '" - ' + str(i[1]) + ' views')
+  db.close()
 
 def add_new(content):
   """Add a post to the 'database' with the current timestamp."""
@@ -19,3 +29,17 @@ def add_new(content):
   c.execute("insert into post values ('%s')" % content)
   db.commit()
   db.close()
+
+def main():
+  print('-------------------------------------')
+  print('----- 3  MOST POPULAR ARTICLES ------')
+  print('-------------------------------------')
+  get_top_3_articles()
+  print('\n')
+  print('-------------------------------------')
+  print('------ 3  MOST POPULAR AUTHORS ------')
+  print('-------------------------------------')
+  get_top_3_authors()
+  print('\n')
+
+main()
